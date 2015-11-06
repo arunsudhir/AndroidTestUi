0399c633222457c573d85e4b5ddea027c622eda5
Arun Sudhir <arunsudhir@gmail.com>
2015-11-06 02:38:59 -0800

    Add code for Material Layout

--- app/src/main/java/com/arunsudhir/androidtestui/MaterialActivity.java
+++ app/src/main/java/com/arunsudhir/androidtestui/MaterialActivity.java
@@ -6,6 +6,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import it.gmariotti.cardslib.library.cards.actions.BaseSupplementalAction;
+import it.gmariotti.cardslib.library.cards.actions.TextSupplementalAction;
+import it.gmariotti.cardslib.library.cards.material.MaterialLargeImageCard;
+import it.gmariotti.cardslib.library.internal.Card;
 
 public class MaterialActivity extends AppCompatActivity {
 
@@ -15,6 +23,50 @@ public class MaterialActivity extends AppCompatActivity {
         setContentView(R.layout.activity_material);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        // Set supplemental actions as text
+        ArrayList<BaseSupplementalAction> actions = new ArrayList<>();
+
+        // Set supplemental actions
+        TextSupplementalAction t1 = new TextSupplementalAction(this, R.id.text1);
+        t1.setOnActionClickListener(new BaseSupplementalAction.OnActionClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+              //  Toast.makeText(this," Click on Text SHARE ",Toast.LENGTH_SHORT).show();
+            }
+        });
+        actions.add(t1);
+
+        TextSupplementalAction t2 = new TextSupplementalAction(this, R.id.text2);
+        t2.setOnActionClickListener(new BaseSupplementalAction.OnActionClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+              //  Toast.makeText(this," Click on Text LEARN ",Toast.LENGTH_SHORT).show();
+            }
+        });
+        actions.add(t2);
+
+        //Create a Card, set the title over the image and set the thumbnail
+        MaterialLargeImageCard card =
+                MaterialLargeImageCard.with(this)
+                        .setTextOverImage("Best of 2015")
+                        .setTitle("The best melodies/chartbusters of 2015")
+                        .setSubTitle("Includes Premam, Oru Vadakkan Selfie, Ennu Ninde Moideen etc")
+                        .useDrawableUrl("http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg")
+                        .setupSupplementalActions(R.layout.material, actions)
+                        .build();
+
+        card.setOnClickListener(new Card.OnCardClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+               // Toast.makeText(this," Click on ActionArea ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
