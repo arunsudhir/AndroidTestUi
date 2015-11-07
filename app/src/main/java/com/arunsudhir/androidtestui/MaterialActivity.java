a4c044ecccce1f24ad65dff4a5584012bde3b00e
arunsudhir <arunsudhir@gmail.com>
2015-11-06 20:13:49 -0800

    One Material layout card working!

--- app/src/main/java/com/arunsudhir/androidtestui/MaterialActivity.java
+++ app/src/main/java/com/arunsudhir/androidtestui/MaterialActivity.java
@@ -1,12 +1,18 @@
 package com.arunsudhir.androidtestui;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -15,9 +21,12 @@ import it.gmariotti.cardslib.library.cards.actions.TextSupplementalAction;
 import it.gmariotti.cardslib.library.cards.material.MaterialLargeImageCard;
 import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
+import it.gmariotti.cardslib.library.internal.CardThumbnail;
 import it.gmariotti.cardslib.library.view.CardGridView;
 import it.gmariotti.cardslib.library.view.CardListView;
 import it.gmariotti.cardslib.library.view.CardViewNative;
+
+import static android.widget.Toast.*;
 
 public class MaterialActivity extends AppCompatActivity {
 
@@ -32,7 +41,7 @@ public class MaterialActivity extends AppCompatActivity {
 
         ArrayList<Card> cards = new ArrayList<>();
 
-
+/*
         // Set supplemental actions as text
         ArrayList<BaseSupplementalAction> actions = new ArrayList<>();
 
@@ -54,29 +63,26 @@ public class MaterialActivity extends AppCompatActivity {
             }
         });
         actions.add(t2);
+*/
 
-        //Create a Card, set the title over the image and set the thumbnail
-        MaterialLargeImageCard card =
-                MaterialLargeImageCard.with(this)
-                        .setTextOverImage("Best of 2015")
-                        .setTitle("The best melodies/chartbusters of 2015")
-                        .setSubTitle("Includes Premam, Oru Vadakkan Selfie, Ennu Ninde Moideen etc")
-                        .useDrawableUrl("http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg")
-                        .setupSupplementalActions(R.layout.material, actions)
-                        .build();
-
-        card.setOnClickListener(new Card.OnCardClickListener() {
-            @Override
-            public void onClick(Card card, View view) {
-               // Toast.makeText(this," Click on ActionArea ", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        cards.add(card);
+        /* cards.add(createCard(this, "Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
+        cards.add(createCard(this,"Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
+        cards.add(createCard(this,"Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
+        cards.add(createCard(this,"Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
+        cards.add(createCard(this,"Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
+        cards.add(createCard(this,"Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
+        cards.add(createCard(this,"Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
+        cards.add(createCard(this,"Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
+        cards.add(createCard(this,"Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
+        cards.add(createCard(this,"Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
+        cards.add(createCard(this,"Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
+        cards.add(createCard(this,"Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
         CardArrayAdapter adap = new CardArrayAdapter(this, cards);
-        CardListView gridView = (CardListView) this.findViewById(R.id.myList);
-        if (gridView!=null){
-            gridView.setAdapter(adap);
+        */
+        CardViewNative cn = (CardViewNative) this.findViewById(R.id.carddemo_largeimage);
+       // CardGridView gridView = (CardGridView) this.findViewById(R.id.myList);
+        if (cn!=null){
+            cn.setCard(createCard(this,"Best of 2015", "The best melodies/chartbusters of 2015", "http://www.mywimbo.com/MalRadio/PlaylistHeroImages/2015_1.jpg"));
         }
 
 
@@ -88,6 +94,46 @@ public class MaterialActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private Card createCard(final Context context, String textOver, String title, final String drawableUrl)
+    {
+        MaterialLargeImageCard card =
+                MaterialLargeImageCard.with(context)
+                        .setTextOverImage("Italian Beaches")
+                        .setTitle("This is my favorite local beach")
+                        .setSubTitle("A wonderful place")
+                        .useDrawableExternal(new MaterialLargeImageCard.DrawableExternal() {
+                            @Override
+                            public void setupInnerViewElements(ViewGroup parent, View viewImage) {
+
+                                Picasso.with(context).setIndicatorsEnabled(true);  //only for debug tests
+                                Picasso.with(context)
+                                        .load(drawableUrl)
+                                       // .error(R.drawable.ic_error_loadingsmall)
+                                        .into((ImageView) viewImage);
+                            }
+                        })
+                        //.setupSupplementalActions(R.layout.carddemo_native_material_supplemental_actions_large, actions)
+                        .build();
+        final Activity act = this;
+        card.setOnClickListener(new Card.OnCardClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+                makeText(act, " Click on ActionArea ", LENGTH_SHORT).show();
+            }
+        });
+        return card;
+    }
+
+    private Card createCard2(Context context, String textOver, String title, String drawableUrl)
+    {
+        Card card = new Card(this, R.layout.card_thumbnail);
+        CardThumbnail cdt = new CardThumbnail(context);
+        cdt.setUrlResource(drawableUrl);
+        card.addCardThumbnail(cdt);
+        card.getCardHeader().setTitle(title);
+        return card;
     }
 
 }
